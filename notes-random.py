aff8e742539df66371f8eafb5815276c98f5ac39
Alan Eatherly <eatherlyae@gmail.com>
2016-08-01 16:01:22 -0500

    added more notes to think on

--- notes-random.py
+++ notes-random.py
@@ -1,22 +1,21 @@
 # Random Notes for the Birdy Board Mastery Exercise
 
-# Users Storage Model
+# Users Storage Model, list containing randomly generated integer for UID and strings of the names
 [RandomGenUID1, 'UserName', 'FullName']
 [RandomGenUID2, 'UserName', 'FullName']
 [RandomGenUID3, 'UserName', 'FullName']
 [RandomGenUID4, 'UserName', 'FullName']
 [RandomGenUID5, 'UserName', 'FullName']
 
-# Public Chirps Storage Model
-# add spot for comments
+# Public Chirps Storage Model, list of strings and integers for the 2 different IDs and dict of list of comments on given message
 
-[ChirpID, UID, 'Public', 'Chirp Message']
-[ChirpID, UID, 'Public', 'Chirp Message']
-[ChirpID, UID, 'Public', 'Chirp Message']
-[ChirpID, UID, 'Public', 'Chirp Message']
-[ChirpID, UID, 'Public', 'Chirp Message']
+[ChirpID, UID, 'Public', 'Chirp Message', {Comments: ['comment 1', 'comment 2']]
+[ChirpID, UID, 'Public', 'Chirp Message', {Comments: ['comment 1', 'comment 2']]
+[ChirpID, UID, 'Public', 'Chirp Message', {Comments: ['comment 1', 'comment 2']]
+[ChirpID, UID, 'Public', 'Chirp Message', {Comments: ['comment 1', 'comment 2']]
+[ChirpID, UID, 'Public', 'Chirp Message', {Comments: ['comment 1', 'comment 2']]
 
-# Private Chirps Storage Model
+# Private Chirps Storage Model, list of strings and integers for the 2 different IDs
 
 [ChirpID, UID of author, 'Private', UID of receiver, 'Chirp Message']
 [ChirpID, UID of author, 'Private', UID of receiver, 'Chirp Message']
